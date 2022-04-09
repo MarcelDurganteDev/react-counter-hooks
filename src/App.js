89be2acbf6a91cc2597dd0ba4c5b7f01e3abd48b
MarcelDurgante.Dev <info@marceldurgante.dev>
2022-04-09 21:07:39 +0200

    React counter with hooks done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App () {
+  
+  const [count, setCount] = useState( 0 );
+
+  function increment () {
+    setCount(count + 1)
+  }
+
+  function decrement() {
+      setCount(count - 1);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </section>
   );
 }
-
-export default App;
